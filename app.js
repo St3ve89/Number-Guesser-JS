7aac644ad605838ac2ceedea4dc1005e239a117c
St3ve89 <istvan.acs89@gmail.com>
2018-05-07 13:16:17 +0100

    Validations and win case set up

---
+++ app.js
@@ -0,0 +1,55 @@
+// GAME FUNCTION:
+// - Player must guess a number between a min and max
+// - Player gets a certain amount of guesses
+// - Notify player of guesses remaining
+// - Notify the player of the correct answer if loose
+// - Let player choose to play again
+
+// GAME values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// listen for guess
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+  console.log(guess);
+
+  // validate our input
+  if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}`, `red`);
+  }
+
+  // check if it is a awinning number
+  if(guess === winningNum) {
+    // disable input
+    guessInput.disabled = true;
+    
+    // change border color
+    guessInput.style.borderColor = 'green';
+
+    // set message
+    setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+  } else {
+
+  }
+});
+
+// set message
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+}
